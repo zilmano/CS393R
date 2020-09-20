e421a98aec7375d9e3c53932ed9327b8c1604a34
oleg zilman <zilmano@gmail.com>
2020-09-20 18:12:36 -0500

    add GenConsts

--- src/navigation/constants.h
+++ src/navigation/constants.h
@@ -7,6 +7,11 @@
 
 #include "xtensor/xtensor.hpp"
 
+namespace GenConsts{
+    const float kEpsilon = 1e-5f;
+    const float step_period = 0.02f;
+}
+
 namespace PhysicsConsts{
     const static float max_acc = 3.0f;
     const static float max_vel = 1.0f;
