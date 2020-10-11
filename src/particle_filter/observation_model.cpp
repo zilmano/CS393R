9543deb6c6376e068548f8d859252070978072c8
oleg zilman <zilmano@gmail.com>
2020-10-10 20:54:02 -0500

    add underscored to observation_model.h/cpp

--- src/particle_filter/observation_model.cpp
+++ src/particle_filter/observation_model.cpp
@@ -9,7 +9,7 @@
 #include "xtensor/xio.hpp"
 #include <iostream>
 
-ObservationModel::ObservationModel(float gamma, float sigma) : gamma(gamma), sigma(sigma) {}
+ObservationModel::ObservationModel(float gamma, float sigma) : gamma_(gamma), sigma_(sigma) {}
 
 float ObservationModel::calculate_accumulated_loglikelihood(simd_vec_type &intersections, simd_vec_type &observations) {
     simd_vec_type log_likelihood;
@@ -17,10 +17,10 @@ float ObservationModel::calculate_accumulated_loglikelihood(simd_vec_type &inter
     xsimd::transform(intersections.begin(), intersections.end(), observations.begin(),
                      log_likelihood.begin(),
                      [&](const auto &intersect, const auto & observ){
-        auto div = (intersect - observ) / sigma;
+        auto div = (intersect - observ) / sigma_;
         return div * div;
     });
     std::cout << std::endl << xt::adapt(log_likelihood) << std::endl;
     float acc = xt::sum(xt::adapt(log_likelihood))[0];
-    return acc * -gamma;
+    return acc * -gamma_;
 }
