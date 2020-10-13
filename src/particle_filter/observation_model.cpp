930ff21d774f8ffce8bf67ba6c396f1b9b2c4b0c
oleg zilman <zilmano@gmail.com>
2020-10-13 01:40:12 -0500

    disable DrawPointCloud of state_estimator, to make things faster

--- src/particle_filter/observation_model.cpp
+++ src/particle_filter/observation_model.cpp
@@ -20,7 +20,8 @@ float ObservationModel::calculate_accumulated_loglikelihood(simd_vec_type &inter
         auto div = (intersect - observ) / sigma_;
         return div * div;
     });
-    std::cout << std::endl << xt::adapt(log_likelihood) << std::endl;
+    //std::cout << "Weight vector:" << std::endl;
+    //std::cout << std::endl << xt::adapt(log_likelihood) << std::endl;
     float acc = xt::sum(xt::adapt(log_likelihood))[0];
     return acc * -gamma_;
 }
