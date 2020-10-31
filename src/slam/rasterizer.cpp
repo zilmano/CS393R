f8696b1dd973d4c97b72e247618ca4c15b53996a
liyanc <liyanchen@acm.org>
2020-10-31 18:07:59 -0500

    loglikelihood

---
+++ src/slam/rasterizer.cpp
@@ -0,0 +1,6 @@
+//
+// Created by liyanc on 10/31/20.
+//
+
+#include "rasterizer.h"
+#include "log_normal.h"
---
+++ src/slam/rasterizer.h
@@ -0,0 +1,15 @@
+//
+// Created by liyanc on 10/31/20.
+//
+
+#ifndef REPO_RASTERIZER_H
+#define REPO_RASTERIZER_H
+
+
+class Rasterizer {
+    Rasterizer(int res_x)
+
+};
+
+
+#endif //REPO_RASTERIZER_H
