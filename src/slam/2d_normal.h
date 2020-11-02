a9abfdb8c9437d4cfe89232ad9f2171bebcaba4f
liyanc <liyanchen@acm.org>
2020-11-02 15:03:55 -0600

    3d_normal

--- src/slam/2d_normal.h
+++ src/slam/2d_normal.h
@@ -10,13 +10,31 @@
 #include "eigen3/Eigen/Geometry"
 
 using Eigen::Vector2f;
+using Eigen::Vector3f;
 using Eigen::Matrix2f;
+using Eigen::Matrix3f;
 
 std::vector<float> loglikelihood_mvn(const std::vector<Vector2f> & x, const Vector2f & mu, const Matrix2f & sigma) {
     Matrix2f precision = sigma.inverse();
     Eigen::CompleteOrthogonalDecomposition<Matrix2f> dec{sigma};
     float logabsdet = dec.logAbsDeterminant();
-    const float norm_const = log(M_PI * 2) - 0.5 * logabsdet;
+    const float norm_const = -logf(M_PI * 2) - 0.5 * logabsdet;
+
+    std::vector<float> res;
+    res.reserve(x.size());
+    for (auto & obs : x) {
+        auto diff = obs - mu;
+        float mahalanobis = diff.transpose() * precision * diff;
+        res.emplace_back(norm_const - mahalanobis);
+    }
+    return res;
+}
+
+std::vector<float> loglikelihood_3d_mvn(const std::vector<Vector3f> & x, const Vector3f & mu, const Matrix3f & sigma) {
+    Matrix3f precision = sigma.inverse();
+    Eigen::CompleteOrthogonalDecomposition<Matrix3f> dec{sigma};
+    float logabsdet = dec.logAbsDeterminant();
+    const float norm_const = -1.5f * logf(M_PI * 2) - 0.5 * logabsdet;
 
     std::vector<float> res;
     res.reserve(x.size());
---
+++ src/slam/motion_model.h
@@ -0,0 +1,8 @@
+//
+// Created by liyanc on 11/1/20.
+//
+
+#ifndef REPO_MOTION_MODEL_H
+#define REPO_MOTION_MODEL_H
+
+#endif //REPO_MOTION_MODEL_H
