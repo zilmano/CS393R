96e0ababb771879fe2941e6a9f474c32bee59674
Liyan Chen <liyanc@wireless-10-147-15-107.public.utexas.edu>
2020-11-25 10:33:33 -0600

    tune

--- src/particle_filter/particle_filter_main.cc
+++ src/particle_filter/particle_filter_main.cc
@@ -337,14 +337,14 @@ int main(int argc, char** argv) {
 
 
   particle_filter::PfParams params;
-    params.radar_dcdownsample_rate = 10;
+    params.radar_downsample_rate = 10;
     params.num_particles = 30;
     params.resample_n_step= 10;
     params.d_long=10.0;
     params.d_short=6.0;
     params.k_4 = 0.1;
     params.k_2 = 0.1;
-    params.sigma_obs = 0.1;
+    params.sigma_obs = 0.5;
     params.gamma = 0.07;
   particle_filter_.SetParams(params);
   particle_filter_.SetRosHandleAndInitPubs(&visualization_publisher_, &vis_msg_);
