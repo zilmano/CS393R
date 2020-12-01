d168327bd50a9a2b1e3bc1ace02f0af762610b61
oleg zilman <zilmano@gmail.com>
2020-12-01 16:56:07 -0600

    update class skeleton

--- src/adaptive_workspace_bias_planning/planning.h
+++ src/adaptive_workspace_bias_planning/planning.h
@@ -10,19 +10,39 @@ public:
      AWBPlanner(){};
      
      void load_map(){};
+     
+     // Do the gradient based learning to find the best weights.
      void train() {};
+     
+     // Use sampling using the weights to generate the graph, and store it in the new graph class.
      void generate_sampled_graph() {};
-     void generate_plan() {}
+     
+     // Use A* to find the best plan in the 3D configuration space
+     void generate_plan() {};
+
+private:
+    //Try to generate edge between two points in config space.
+    void generateEdge() {};
+
+    // our line sampling from assigment 2 goes here.
+    void sample_from_disribution() {}; 
+
+
 
 private:
     Graph map_workspace_;
     A_star ws_planner_;
+    
+    // OLEG: This has to be a modified A* implementation so that it works with the new graph class that represent the sampled graph.
     A_star config_space_planner_;
 
+    // Vector holding our weights used for the Gibbs sampling
     Eigen::VectorXd weights_;
-    //To Add: Features class "FeatureCalc", can take the astar ws_planner as an argument to contructor 
+
+    //To Add member: Features class "FeatureCalc", can take the astar ws_planner as an argument to contructor 
     //        or function generate the features to one of it's function.
 
+    //To add member: Class to store the sampled config space graph.
      
 };
 
